70b69e28dc53ced098373de6bc7056943ea04be4
Pakybot <159849422+Pakybot@users.noreply.github.com>
2024-02-13 00:12:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['13478424835', 'me', true],
+  ['13478424835', 'paky bot', true], 
   [''] 
 ] //Number of owners
 
